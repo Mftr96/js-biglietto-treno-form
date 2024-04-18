517c79d9136430a01286a8ac0845ab89aae26262
Mftr96 <matteo.federici96@gmail.com>
2024-04-18 17:03:41 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//condizioni iniziali
+
+const prezo_km=0.21;
+
+//richiesta dati utente 
+const km_passeggero=document.getElementById("km-input");
+
+const eta_passeggero=document.getElementById("age-input");
+console.log('SINCRONO', 'eta_passegero.value', eta_passeggero.value, typeof eta_passeggero.value);
+
+
+const my_button=document.getElementById("bt-form");
+
+
+my_button.addEventListener('click',function(){
+
+  console.log(eta_passeggero.value);
+  console.log(km_passeggero_value);
+  const prezzo_base= prezzo_km * km_passeggero.value;
+  console.log(prezzo_base.value);
+
+
+})
+
+

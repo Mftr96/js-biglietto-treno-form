bc90d5cec3837f4146cf2b45f32b8d5ef72fad0c
Mftr96 <matteo.federici96@gmail.com>
2024-04-18 17:11:38 +0200

    in cerca del prezzo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 //condizioni iniziali
 
-const prezo_km=0.21;
+const prezzo_km=0.21;
 
 //richiesta dati utente 
 const km_passeggero=document.getElementById("km-input");
 
 const eta_passeggero=document.getElementById("age-input");
 console.log('SINCRONO', 'eta_passegero.value', eta_passeggero.value, typeof eta_passeggero.value);
+const prezzo_base= prezzo_km * km_passeggero.value;
+console.log(prezzo_base.value);
 
 
 const my_button=document.getElementById("bt-form");
@@ -18,7 +20,6 @@ my_button.addEventListener('click',function(){
   console.log(km_passeggero_value);
   const prezzo_base= prezzo_km * km_passeggero.value;
   console.log(prezzo_base.value);
-
 
 })
 
